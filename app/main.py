6f41a4226731fbd9a57cc444231499329fe94f03
hoatrancntt <hoatracntt@gmail.com>
2025-11-28 01:11:54 +0700

    edit main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins - Harbor - ArgoCD Pipeline!"
+    return "-Vanhoa-------Hello from Jenkins - Harbor - ArgoCD Pipeline!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
